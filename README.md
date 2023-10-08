3ddeb4bb4b32a4209c410e9ff5ab67dc5e081649
EZRA-DVLPR <derpisontr@gmail.com>
2023-10-08 12:12:48 -0700

    Added structure and group project

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # comp_542_proj
 
 A collection of assignments for my Machine Learning Class at CSUN Fall 2023
+
+# Explanations of each folder:
+
+## assignment2
+
+A short assignment to learn the basics of ML with the well-known iris dataset from UCI
+
+## group_proj
+
+A group project where me and my groupmate are working on the same dataset and problem, but are working with different algorithms.
---
+++ group_proj/helper.py
@@ -0,0 +1,46 @@
+#helper for the project jupyter notebook
+
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
+
+#given a filepath to a folder of images 
+#returns maxWidth, minWidth, maxHeight, minHeight
+def getMaxMin(folder):
+    maxWidth = [-1, 0]
+    minWidth = [-1, 0]
+
+    maxHeight = [0, -1]
+    minHeight = [0, -1]
+
+    onlyfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
+
+    #iteratively find max/min width/height of images
+    for filename in onlyfiles:
+        with Image.open(folder + filename) as img:
+            width, height = img.size
+
+        #update maxWidth/maxHeight if new max is found
+        if width > maxWidth[0]:
+            maxWidth = img.size
+
+        if height > maxHeight[1]:
+            maxHeight = img.size
+
+
+        #initialize minWidth/minHeight to first encountered image
+        if minWidth[0] < 0:
+            minWidth = img.size
+
+        if minHeight[1] < 0:
+            minHeight = img.size
+
+
+        #update minWidth/minHeight if new min is found
+        if width < minWidth[0]:
+            minWidth = img.size
+
+        if height < minHeight[1]:
+            minHeight = img.size
+
+    return maxWidth, minWidth, maxHeight, minHeight
